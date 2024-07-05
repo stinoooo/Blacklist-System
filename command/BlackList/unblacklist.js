a91c7995b5039f256e72dbb29004633bfb7dd193
stino <stabbajabba@gmail.com>
2024-07-05 03:04:55 +0200

    Update unblacklist.js

--- command/BlackList/unblacklist.js
+++ command/BlackList/unblacklist.js
@@ -1,66 +1,38 @@
-/*
-             ________________________________________________
-            /                                                \
-           |    _________________________________________     |
-           |   |                                         |    |
-           |   |  KMCODES BlackList System By KINGMAN    |    |
-           |   |                                         |    |
-           |   |       Devloper ["Muhammad Kurkar"]      |    |
-           |   |                                         |    |
-           |   |      Phone Number ["+962792914245"]     |    |
-           |   |                                         |    |
-           |   |      All rights reserved to KIGNAMN     |    |
-           |   |                                         |    |
-           |   |  If there is any error, just visit the  |    |
-           |   |                                         |    |
-           |   |        KINGMANDEV Discord Server        |    |
-           |   |                                         |    |
-           |   |_________________________________________|    |
-           |                                                  |
-            \_________________________________________________/
-                   \___________________________________/
-                ___________________________________________
-             _-'    .-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.  --- `-_
-          _-'.-.-. .---.-.-.-.-.-.-.-.-.-.-.-.-.-.-.--.  .-.-.`-_
-       _-'.-.-.-. .---.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-`__`. .-.-.-.`-_
-    _-'.-.-.-.-. .-----.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-----. .-.-.-.-.`-_
- _-'.-.-.-.-.-. .---.-. .-------------------------. .-.---. .---.-.-.-.`-_
-:-------------------------------------------------------------------------:
-`---._.-------------------------------------------------------------._.---'
-*/
-const bl = require('../../me-modals/blacklist/blacklisted')
-const me = require('../../me-config.json')
-const devs = me.dev
+const blacklist = require('../../me-modals/blacklist/blacklisted');
+const config = require('../../me-config.json');
+const administrationTeam = config.dev;
 const { MessageEmbed } = require("discord.js");
- module.exports = {
-   name: "unblacklist",
-   category: "Security",
-   description: "\`لازالة العضوا من القائمة بلاكليست\`",
-   run: async (client, kmsg, args, PREFIX) => {
-    if (!devs.includes(kmsg.author.id)) {
-        return kmsg.reply(`
-        > **\⛔ Failed to execute Command ** 
-        > **Reason:This Command is only for the bot Owners  **
+
+module.exports = {
+    name: "unblacklist",
+    category: "Security",
+    description: "Removes a user from the blacklist.",
+    run: async (client, message, args, PREFIX) => {
+        if (!administrationTeam.includes(message.author.id)) {
+            return message.reply(`
+> **⛔ Command Execution Failed** 
+> **Reason: This command is restricted to the BUK Administration Team.**
+            `);
+        }
+
+        const userId = args[0];
+        if (!userId) {
+            return message.reply(`
+> **⛔ Command Execution Failed** 
+> **Reason: You must provide the user ID.**
+            `);
+        }
+
+        const data = await blacklist.findOneAndDelete({ UserID: userId });
+        if (!data) {
+            return message.reply(`
+> **⛔ Command Execution Failed** 
+> **Reason: This user is not on the blacklist.**
+            `);
+        }
+
+        message.channel.send(`
+> 🌐 **The user has been removed from the blacklist.**
         `);
     }
-    let userid = args[0];
-    if (!userid) {
-        return kmsg.reply(`
-        > **\⛔ Failed to execute Command **
-        > **Reason: You must enter the ID **
-        `)
-    }
-    let DATA = await bl.findOneAndDelete({
-        UserID: userid
-    })
-    if(!DATA) {
-        return kmsg.reply(`
-        > **\⛔ Failed to execute Command **
-        > **Reason: This person isn't on the blacklist  **
-        `)
-    }
-    kmsg.channel.send(`
-    > 🌐**The person has been removed from the blacklist **
-    `)
-    }
- }
+};
