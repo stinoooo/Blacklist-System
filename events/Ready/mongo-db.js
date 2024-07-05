4dbbd6e4c6a6030e34c45092bee7d7c99cd8fab9
stino <stabbajabba@gmail.com>
2024-07-05 02:59:39 +0200

    Update mongo-db.js

--- events/Ready/mongo-db.js
+++ events/Ready/mongo-db.js
@@ -1,45 +1,18 @@
-/*
-             ________________________________________________
-            /                                                \
-           |    _________________________________________     |
-           |   |                                         |    |
-           |   |  kingman BlackList System By KINGMAN    |    |
-           |   |                                         |    |
-           |   |       Devloper ["Muhammad Kurkar"]      |    |
-           |   |                                         |    |
-           |   |      Phone Number ["+962792914245"]     |    |
-           |   |                                         |    |
-           |   |      All rights reserved to KIGNAMN     |    |
-           |   |                                         |    |
-           |   |  If there is any error, just visit the  |    |
-           |   |                                         |    |
-           |   |      KINGMANDEV Discord Server          |    |
-           |   |                                         |    |
-           |   |_________________________________________|    |
-           |                                                  |
-            \_________________________________________________/
-                   \___________________________________/
-                ___________________________________________
-             _-'    .-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.  --- `-_
-          _-'.-.-. .---.-.-.-.-.-.-.-.-.-.-.-.-.-.-.--.  .-.-.`-_
-       _-'.-.-.-. .---.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-`__`. .-.-.-.`-_
-    _-'.-.-.-.-. .-----.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-----. .-.-.-.-.`-_
- _-'.-.-.-.-.-. .---.-. .-------------------------. .-.---. .---.-.-.-.`-_
-:-------------------------------------------------------------------------:
-`---._.-------------------------------------------------------------._.---'
-*/
 const colors = require("colors");
 const mongoose = require('mongoose');
 const discord = require("discord.js");
-const MONGO_DDB = process.env["ME_MONGO"]
+const MONGO_DDB = process.env["ME_MONGO"];
+
 module.exports = {
-	name: 'ready',
-async execute(client) {
-    mongoose.connect(MONGO_DDB, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false}).then(console.log(`
+    name: 'ready',
+    async execute(client) {
+        mongoose.connect(MONGO_DDB, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+            .then(() => console.log(`
 _______________________________
 
-[✅]==> Mongo-DB Connection is OK 
+[✅] MongoDB Connection Successful 
 _______________________________
-    `.brightGreen));
-}
-}
+        `.brightGreen))
+            .catch(err => console.error('MongoDB Connection Error:', err));
+    }
+};
