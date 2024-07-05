2ece5605fc52945cab01b0de54041a1d8acebae8
stino <stabbajabba@gmail.com>
2024-07-05 02:58:50 +0200

    Update command.js

--- me-handler/command.js
+++ me-handler/command.js
@@ -1,27 +1,29 @@
-
 const { readdirSync } = require("fs");
 const ascii = require("ascii-table");
-const fs = require("fs")
 const colors = require("colors");
+
 let table = new ascii("Commands");
-table.setHeading("Command", "Load status");
+table.setHeading("Command", "Load Status");
 
 module.exports = (client) => {
     readdirSync("./command/").forEach(dir => {
-        const commands = readdirSync(`./command/${dir}/`).filter(file => file.endsWith(".js"));
+        const commandFiles = readdirSync(`./command/${dir}/`).filter(file => file.endsWith(".js"));
 
-        for (let file of commands) {
-            let pull = require(`../command/${dir}/${file}`);
+        for (const file of commandFiles) {
+            const command = require(`../command/${dir}/${file}`);
     
-            if (pull.name) {
-                client.commands.set(pull.name, pull);
+            if (command.name) {
+                client.commands.set(command.name, command);
                 table.addRow(file, '✅');
+
+                if (command.aliases && Array.isArray(command.aliases)) {
+                    command.aliases.forEach(alias => client.aliases.set(alias, command.name));
+                }
             } else {
-                table.addRow(file, `❌`);
-                continue;
+                table.addRow(file, '❌');
             }
-            if (pull.aliases && Array.isArray(pull.aliases)) pull.aliases.forEach(alias => client.aliases.set(alias, pull.name));
         }
     });
-    console.log(table.toString().brightCyan);    
-}
+
+    console.log(table.toString().brightCyan);
+};
