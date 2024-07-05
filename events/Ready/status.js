8d993b96497ba488894d4b09c5829ed37a6686a0
stino <stabbajabba@gmail.com>
2024-07-05 03:00:37 +0200

    Update status.js

--- events/Ready/status.js
+++ events/Ready/status.js
@@ -1,53 +1,23 @@
-/*
-             ________________________________________________
-            /                                                \
-           |    _________________________________________     |
-           |   |                                         |    |
-           |   |  KINGMANDEV BlackList System            |    |
-           |   |                                         |    |
-           |   |       Devloper ["Muhammad Kurkar"]      |    |
-           |   |                                         |    |
-           |   |      Phone Number ["+962792914245"]     |    |
-           |   |                                         |    |
-           |   |      All rights reserved to KIGNAMN     |    |
-           |   |                                         |    |
-           |   |  If there is any error, just visit the  |    |
-           |   |                                         |    |
-           |   |         KINGMANDEV Discord Server       |    |
-           |   |                                         |    |
-           |   |_________________________________________|    |
-           |                                                  |
-            \_________________________________________________/
-                   \___________________________________/
-                ___________________________________________
-             _-'    .-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.  --- `-_
-          _-'.-.-. .---.-.-.-.-.-.-.-.-.-.-.-.-.-.-.--.  .-.-.`-_
-       _-'.-.-.-. .---.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-`__`. .-.-.-.`-_
-    _-'.-.-.-.-. .-----.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-----. .-.-.-.-.`-_
- _-'.-.-.-.-.-. .---.-. .-------------------------. .-.---. .---.-.-.-.`-_
-:-------------------------------------------------------------------------:
-`---._.-------------------------------------------------------------._.---'
-*/
 const colors = require("colors");
 const figlet = require('figlet');
+
 module.exports = {
-	name: 'ready',
-async	execute(client) {
-    figlet('KINGMAN', function(err, data) {
-        if (err) {
-            console.log('Something went wrong...');
-            console.dir(err);
-            return;
-        }
-        console.log(data.brightRed)
-    });
-    client.user.setPresence({
-      status: 'online',
-      activity:{
-        name:'KINGMAN ANTICOPYPASTER SYSTEM',
-        type:'STREAMING',
-        url: 'https://www.twitch.tv/kingman4hack'
-      }
-    })
-	}
-}
+    name: 'ready',
+    async execute(client) {
+        figlet('bossman', (err, data) => {
+            if (err) {
+                console.error('An error occurred with figlet:', err);
+                return;
+            }
+            console.log(data.brightRed);
+        });
+
+        client.user.setPresence({
+            status: 'online',
+            activity: {
+                name: 'BUK Administration Team',
+                type: 'LISTENING',
+            }
+        });
+    }
+};
