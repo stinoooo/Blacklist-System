827179f704e31fda486ad202f855a26ca5ad6add
stino <stabbajabba@gmail.com>
2024-07-05 02:54:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,7 @@
-/*
-             ________________________________________________
-            /                                                \
-           |    _________________________________________     |
-           |   |                                         |    |
-           |   |  KMCODES BlackList System By KINGMAN    |    |
-           |   |                                         |    |
-           |   |       Devloper ["Muhammad Kurkar"]      |    |
-           |   |                                         |    |
-           |   |      Phone Number ["+962792914245"]     |    |
-           |   |                                         |    |
-           |   |      All rights reserved to KIGNAMN     |    |
-           |   |                                         |    |
-           |   |  If there is any error, just visit the  |    |
-           |   |                                         |    |
-           |   |        KINGMANDEV Discord Server        |    |
-           |   |                                         |    |
-           |   |_________________________________________|    |
-           |                                                  |
-            \_________________________________________________/
-                   \___________________________________/
-                ___________________________________________
-             _-'    .-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.  --- `-_
-          _-'.-.-. .---.-.-.-.-.-.-.-.-.-.-.-.-.-.-.--.  .-.-.`-_
-       _-'.-.-.-. .---.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-`__`. .-.-.-.`-_
-    _-'.-.-.-.-. .-----.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-----. .-.-.-.-.`-_
- _-'.-.-.-.-.-. .---.-. .-------------------------. .-.---. .---.-.-.-.`-_
-:-------------------------------------------------------------------------:
-`---._.-------------------------------------------------------------._.---'
-*/
 const Discord = require("discord.js");
 const discord = require("discord.js");
 const kingman = require("./alive/keep_work.js")
-const { MessageEmbed  ,  Collection , Client } = require("discord.js");
+const { MessageEmbed, Collection, Client } = require("discord.js");
 const client = new Client()
 client.commands = new discord.Collection();
 client.eventss = new discord.Collection();
@@ -53,10 +23,10 @@ client.on('message', kmsg => {
   }
   if (kmsg.author.bot) return;
   if (!kmsg.guild) {
-    return kmsg.reply("**ONLY WORK ON SERVERS NOT DM**")
+    return kmsg.reply("This only works on servers, not in direct messages.")
   }
   let prefix = PREFIX;
-const args1 = kmsg.content.slice(prefix.length).split(/ +/);
+  const args1 = kmsg.content.slice(prefix.length).split(/ +/);
   if (!kmsg.content.startsWith(PREFIX)) return;
   const args = kmsg.content
     .slice(PREFIX.length)
@@ -66,9 +36,7 @@ const args1 = kmsg.content.slice(prefix.length).split(/ +/);
   if (kmcommand.length === 0) return;
   let kmcode = client.commands.get(kmcommand);
   if (!kmcode) kmcode = client.commands.get(client.aliases.get(kmcommand));
-  if (kmcode) kmcode.run(client, kmsg, args, PREFIX , prefix);
+  if (kmcode) kmcode.run(client, kmsg, args, PREFIX, prefix);
 });
 
-
 client.login(TOKEN_BOT)
-
