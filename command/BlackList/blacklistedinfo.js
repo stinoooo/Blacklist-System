4d643c9dc47755bcf4c21c18479aab607f8dfb82
stino <stabbajabba@gmail.com>
2024-07-05 03:14:23 +0200

    Update blacklistedinfo.js

--- command/BlackList/blacklistedinfo.js
+++ command/BlackList/blacklistedinfo.js
@@ -2,35 +2,41 @@ const blacklist = require('../../me-modals/blacklist/blacklisted');
 const { MessageEmbed } = require("discord.js");
 
 module.exports = {
-    name: "bl-info",
+    name: "blacklistedinfo",
     category: "Security",
     description: "Displays information about a blacklisted user.",
     run: async (client, message, args, PREFIX) => {
         if (!args[0]) {
-            return message.reply(`
+            const embed = new MessageEmbed()
+                .setDescription(`
 > **⛔ Command Execution Failed** 
 > **Reason: You must provide the user ID.**
-            `);
+                `)
+                .setColor("#002a7b");
+            return message.reply({ embeds: [embed] });
         }
 
         const data = await blacklist.findOne({ UserID: args[0] });
         if (!data) {
-            return message.reply(`
+            const embed = new MessageEmbed()
+                .setDescription(`
 > **⛔ Command Execution Failed** 
 > **Reason: This user is not on the blacklist.**
-            `);
+                `)
+                .setColor("#002a7b");
+            return message.reply({ embeds: [embed] });
         }
 
         const infoEmbed = new MessageEmbed()
             .setTitle(`Blacklisted User Info`)
             .setDescription(`Details of the blacklisted user.`)
-            .setColor("GREEN")
+            .setColor("#002a7b")
             .addFields(
                 { name: '**ID**', value: data.UserID, inline: false },
                 { name: '**Reason**', value: data.Reason, inline: false },
                 { name: '**Time**', value: data.Time.toISOString().split('T')[0], inline: false },
             );
 
-        message.channel.send(infoEmbed);
+        message.channel.send({ embeds: [infoEmbed] });
     }
 };
