87b11a50431cb845403ebec8beb900bf7533dfb5
stino <stabbajabba@gmail.com>
2024-07-05 02:58:15 +0200

    Update events.js

--- me-handler/events.js
+++ me-handler/events.js
@@ -1,27 +1,25 @@
-
 const { readdirSync } = require("fs");
 const ascii = require("ascii-table");
-const fs = require("fs")
 const colors = require("colors");
+
 let table = new ascii("Events");
-table.setHeading("Events", "Load status");
+table.setHeading("Event", "Load Status");
+
 module.exports = (client) => {
     readdirSync("./events/").forEach(dir => {
-      const events = readdirSync(`./events/${dir}/`).filter(file => file.endsWith(".js"));
-        for (const file of events){
-        const event = require(`../events/${dir}/${file}`)
-        if(event.name){
-        let eventName = file.split(".")[0];
-        client.eventss.set(event.name, event);
-        table.addRow(file, '✅');
-        client.on(event.name, (...args) => event.execute(...args, client));
-        }else{
-          table.addRow(file, `❌`);
-          continue;
+        const eventFiles = readdirSync(`./events/${dir}/`).filter(file => file.endsWith(".js"));
+
+        for (const file of eventFiles) {
+            const event = require(`../events/${dir}/${file}`);
+            if (event.name) {
+                client.eventss.set(event.name, event);
+                table.addRow(file, '✅');
+                client.on(event.name, (...args) => event.execute(...args, client));
+            } else {
+                table.addRow(file, '❌');
+            }
         }
-      }
     });
-    console.log(table.toString().brightMagenta);    
 
-
-}
+    console.log(table.toString().brightMagenta);
+};
